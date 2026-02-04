fc24a87d85be570b19872cbf7312c6aa5ccf59e0
Michał Miotła <miotlamichal@gmail.com>
2026-02-04 16:10:57 +0100

    working with assortment  section

--- src/app/components/Assortment/AssortmentSection.js
+++ src/app/components/Assortment/AssortmentSection.js
@@ -1,6 +1,8 @@
+import Image from 'next/image'
 import Container from '../Container/Container'
 import SectionTitle from '../SectionTitle/SectionTitle'
 import styles from './AssortmentSection.module.css'
+import assortmentData from '@/app/data/AssortmentData'
 
 export default function AssortmentSection() {
 	return (
@@ -10,19 +12,22 @@ export default function AssortmentSection() {
 				<div className={styles.assortment_container}>
 					<div className={styles.list}>
 						<ul>
-							<li>Prasa</li>
-							<li>Słodycze</li>
-							<li>Napoje</li>
-							<li>Papierosy</li>
-							<li>Tytonie</li>
-							<li>Liquidy</li>
-							<li>Bilety</li>
-							<li>Kartki okolicznościowe</li>
-							<li>Papierosy</li>
-							<li>Tytonie</li>
-							<li>Liquidy</li>
-							<li>Bilety</li>
-							<li>Kartki okolicznościowe</li>
+							{assortmentData.map(assortment => (
+								<li key={assortment.index}>
+									<div className={styles.button_details}>
+										<Image alt={assortment.alt} src={assortment.icon} width={40} height={40}></Image>
+										<p>{assortment.title}</p>
+									</div>
+									<div className={styles.button_right}>
+										<Image
+											className={styles.img_right}
+											alt='icon right'
+											src='/right.svg'
+											width={40}
+											height={40}></Image>
+									</div>
+								</li>
+							))}
 						</ul>
 					</div>
 					<div className={styles.details}>123</div>
---
+++ src/app/data/AssortmentData.js
@@ -0,0 +1,74 @@
+const assortmentData = [
+	{
+		index: 0,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+	{
+		index: 1,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+	{
+		index: 2,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+	{
+		index: 3,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+	{
+		index: 4,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+	{
+		index: 5,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+	{
+		index: 6,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+	{
+		index: 7,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+	{
+		index: 8,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+	{
+		index: 9,
+		title: 'Prasa',
+		icon: '/press.svg',
+		alt: 'press icon',
+		description: 'asdbadsbasd adsjkbasdbjad sadjbajksdbajsd ddmajdada',
+	},
+]
+
+export default assortmentData
