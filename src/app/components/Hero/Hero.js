b07d45b22299928d362577d13503e8d692adc382
Michał Miotła <miotlamichal@gmail.com>
2026-03-09 16:32:20 +0100

    reworking hero section

--- src/app/components/Hero/Hero.js
+++ src/app/components/Hero/Hero.js
@@ -1,22 +1,58 @@
+'use client'
+
 import Container from '../Container/Container'
 import styles from './Hero.module.css'
 import Image from 'next/image'
-import HeroImg from '../../../../public/newsstand.svg'
+import HeroNewsstand from '../../../../public/newsstand.svg'
+import HeroReading from '../../../../public/reading.svg'
+import { useEffect, useState } from 'react'
+import Button from '../Button/Button'
 
 export default function Hero() {
+	const [currentSlide, setCurrentSlide] = useState(0)
+
+	const slides = [
+		{
+			image: HeroNewsstand,
+			heading: 'twoje centrum informacji i drobnych przyjemności :)',
+		},
+		{
+			image: HeroReading,
+			headingOne: 'strona dla klientów Saloników Prasowych Kolporter',
+			headingTwo: 'sprawdź promocje i odwiedź najbliższy punkt',
+		},
+	]
+
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentSlide(prev => (prev + 1) % slides.length)
+		}, 5000) // zmiana co 5s
+
+		return () => clearInterval(interval)
+	}, [])
+
+	const slide = slides[currentSlide]
+
 	return (
 		<div className={styles.hero}>
 			<Container>
 				<div className={styles.main}>
 					<div className={styles.main_titles}>
-						<h1 className={styles.title}>twoje centrum informacji i drobnych przyjemności :)</h1>
-						<h2>
-							strona dla klientów Saloników Prasowych <strong>Kolporter</strong>.
-						</h2>
-						<h2>sprawdź najświeższe informacje oraz promocje i odwiedź swój najbliższy punkt!</h2>
+						{currentSlide === 0 ? (
+							<>
+								<h1 className={styles.title}>{slides[0].heading}</h1>
+								<Button title={'Sprawdź aktualności'} />
+							</>
+						) : (
+							<>
+								<h2>{slides[1].headingOne}</h2>
+								<h2>{slides[1].headingTwo}</h2>
+								<Button title={'Znajdź Salonik'} />
+							</>
+						)}
 					</div>
 					<div className={styles.main_image_box}>
-						<Image className={styles.main_image} src={HeroImg} alt='newsstand hero illustration' />
+						<Image className={styles.main_image} src={slide.image} alt='newsstand hero illustration' />
 					</div>
 				</div>
 			</Container>
