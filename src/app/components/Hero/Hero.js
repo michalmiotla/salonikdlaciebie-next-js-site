23581336332dd567515ac87093f2e15c6e92b5b5
Michał Miotła <miotlamichal@gmail.com>
2026-03-11 15:49:12 +0100

    reworking hero section

--- src/app/components/Hero/Hero.js
+++ src/app/components/Hero/Hero.js
@@ -5,31 +5,38 @@ import styles from './Hero.module.css'
 import Image from 'next/image'
 import HeroNewsstand from '../../../../public/newsstand.svg'
 import HeroReading from '../../../../public/reading.svg'
+import HeroDiscount from '../../../../public/discount.svg'
+import HeroGaming from '../../../../public/gaming.svg'
 import { useEffect, useState } from 'react'
 import Button from '../Button/Button'
+import Logo from '../../../../public/Kolporterbackground.png'
 
 export default function Hero() {
 	const [currentSlide, setCurrentSlide] = useState(0)
 
 	const slides = [
-		{
-			image: HeroNewsstand,
-			heading: 'twoje centrum informacji i drobnych przyjemności :)',
-		},
-		{
-			image: HeroReading,
-			headingOne: 'strona dla klientów Saloników Prasowych Kolporter',
-			headingTwo: 'sprawdź promocje i odwiedź najbliższy punkt',
-		},
+		HeroNewsstand,
+		HeroReading,
+		HeroDiscount,
+		HeroGaming,
+		// {
+		// 	image: HeroNewsstand,
+		// 	heading: 'Kolporter - twoje centrum informacji i drobnych przyjemności :)',
+		// },
+		// {
+		// 	image: HeroReading,
+		// 	headingOne: 'strona dla klientów Saloników Prasowych Kolporter',
+		// 	headingTwo: 'sprawdź promocje i odwiedź najbliższy punkt',
+		// },
 	]
 
-	// useEffect(() => {
-	// 	const interval = setInterval(() => {
-	// 		setCurrentSlide(prev => (prev + 1) % slides.length)
-	// 	}, 5000) // zmiana co 5s
+	useEffect(() => {
+		const interval = setInterval(() => {
+			setCurrentSlide(prev => (prev + 1) % slides.length)
+		}, 10000) // zmiana co 5s
 
-	// 	return () => clearInterval(interval)
-	// }, [])
+		return () => clearInterval(interval)
+	}, [])
 
 	const slide = slides[currentSlide]
 
@@ -38,7 +45,12 @@ export default function Hero() {
 			<Container>
 				<div className={styles.main}>
 					<div className={styles.main_titles}>
-						{currentSlide === 0 ? (
+						{/* <Image className={styles.logo} src={Logo} alt='newsstand hero illustration' /> */}
+						<h1 className={styles.title}>Kolporter - twoje centrum informacji i drobnych przyjemności :)</h1>
+						<Button color={'yellow'} title={'sprawdź aktualności'} />
+						<Button color={'white'} title={'zobacz promocje'} />
+
+						{/* {currentSlide === 0 ? (
 							<>
 								<h1 className={styles.title}>{slides[0].heading}</h1>
 								<Button color={'yellow'} title={'sprawdź aktualności'} />
@@ -50,10 +62,10 @@ export default function Hero() {
 								<h2>{slides[1].headingTwo}</h2>
 								<Button title={'znajdź salonik'} />
 							</>
-						)}
+						)} */}
 					</div>
 					<div className={styles.main_image_box}>
-						<Image className={styles.main_image} src={slide.image} alt='newsstand hero illustration' />
+						<Image className={styles.main_image} src={slides[currentSlide]} alt='newsstand hero illustration' />
 					</div>
 				</div>
 			</Container>
