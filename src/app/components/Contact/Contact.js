3820cd086ca035384ad1d70a2802a440fe3f1090
Michał Miotła <miotlamichal@gmail.com>
2026-04-18 18:26:48 +0200

    working with footer section

--- src/app/components/Contact/Contact.js
+++ src/app/components/Contact/Contact.js
@@ -1,9 +1,47 @@
+'use client'
+
+import Link from 'next/link'
 import Container from '../Container/Container'
 import Map from '../Map/Map'
 import SectionTitle from '../SectionTitle/SectionTitle'
 import styles from './Contact.module.css'
+import { useState } from 'react'
 
 export default function Contact() {
+	const [copiedMail, setCopiedMail] = useState(false)
+	const [copiedPhone, setCopiedPhone] = useState(false)
+
+	const mail = 'biuro@kolporter.com.pl'
+	const phone = '+48 41 367 88 88'
+
+	const CopyMail = async () => {
+		try {
+			await navigator.clipboard.writeText(mail)
+
+			setCopiedMail(true)
+
+			setTimeout(() => {
+				setCopiedMail(false)
+			}, 2000)
+		} catch (error) {
+			console.error('Błąd kopiowania', error)
+		}
+	}
+
+	const CopyPhone = async () => {
+		try {
+			await navigator.clipboard.writeText(phone)
+
+			setCopiedPhone(true)
+
+			setTimeout(() => {
+				setCopiedPhone(false)
+			}, 2000)
+		} catch (error) {
+			console.error('Błąd kopiowania', error)
+		}
+	}
+
 	return (
 		<Container>
 			<section className={styles.contact_section} id='kontakt'>
@@ -12,31 +50,49 @@ export default function Contact() {
 				<div className={styles.contact_container}>
 					<p className={styles.title}>Oficjalne źródła i kontakt *</p>
 					<div className={styles.box}>
-						<p className={styles.site}>kolporter.com.pl</p>
+						<Link target='_blank' href={'https://www.kolporter.com.pl'} className={styles.site}>
+							kolporter.com.pl
+						</Link>
 						<p>Oficjalna strona firmy Kolporter Sp. z o.o.</p>
 					</div>
 					<div className={styles.box}>
-						<p className={styles.site}>news.kolporter.com.pl</p>
+						<Link target='_blank' href={'https://news.kolporter.com.pl'} className={styles.site}>
+							news.kolporter.com.pl
+						</Link>
 						<p>Aktualności z życia firmy</p>
 					</div>
 					<div className={styles.box}>
-						<p className={styles.site}>dostawa.kolporter.com.pl</p>
+						<Link target='_blank' href={'https://dostawa.kolporter.com.pl'} className={styles.site}>
+							dostawa.kolporter.com.pl
+						</Link>
 						<p>Lista tytułów w dzisiejszej dostawie prasy</p>
 					</div>
 					<div className={styles.box}>
-						<p className={styles.site}>rynekprasowy.pl</p>
+						<Link target='_blank' href={'https://www.rynekprasowy.pl'} className={styles.site}>
+							rynekprasowy.pl
+						</Link>
 						<p>Aktualności z rynku prasowego</p>
 					</div>
 					<div className={styles.box}>
-						<p className={styles.site}>twojsalonik.pl</p>
+						<Link target='_blank' href={'https://twojsalonik.pl'} className={styles.site}>
+							twojsalonik.pl
+						</Link>
 						<p>Aktualne oferty pracy</p>
 					</div>
 					<div className={styles.box}>
-						<p className={styles.site}>biuro@kolporter.com.pl</p>
+						<p onClick={CopyMail} className={styles.site}>
+							{mail}
+							{copiedMail && <span className={styles.alert}>Skopiowano!</span>}
+						</p>
+
 						<p>Mailowy adres kontaktowy</p>
 					</div>
 					<div className={styles.box}>
-						<p className={styles.site}>+48 41 367 88 88</p>
+						<p onClick={CopyPhone} className={styles.site}>
+							{phone}
+							{copiedPhone && <span className={styles.alert}>Skopiowano!</span>}
+						</p>
+
 						<p>Numer infolinii biura </p>
 					</div>
 					<p className={styles.description}>
--- src/app/components/Footer/Footer.js
+++ src/app/components/Footer/Footer.js
@@ -3,13 +3,32 @@
 import Image from 'next/image'
 import styles from './Footer.module.css'
 import Link from 'next/link'
-import LOGO from '../../../../public/logo.png'
+import LOGO from '../../../../public/logo-white.png'
 import FB from '../../../../public/fb.svg'
 import INSTA from '../../../../public/insta.svg'
 import LINKED from '../../../../public/linked.svg'
 import Container from '../Container/Container'
+import { useState } from 'react'
 
 export default function Footer() {
+	const [copied, setCopied] = useState(false)
+
+	const email = 'miotlamichal@gmail.com'
+
+	const handleCopy = async () => {
+		try {
+			await navigator.clipboard.writeText(email)
+
+			setCopied(true)
+
+			setTimeout(() => {
+				setCopied(false)
+			}, 2000)
+		} catch (error) {
+			console.error('Błąd kopiowania', error)
+		}
+	}
+
 	function backToTop() {
 		setIsMobileNavShown(false)
 		window.scrollTo({ top: 0, behavior: 'smooth' })
@@ -27,14 +46,14 @@ export default function Footer() {
 					<div className={styles.socials_container}>
 						<p>KOLPORTER w mediach społecznościowych:</p>
 						<div className={styles.socials}>
-							<Link onClick={backToTop} href='/'>
-								<Image width={50} height={50} src={FB} alt='salonikdlaciebie.pl' />
+							<Link target='_blank' href='https://www.kolporter.com.pl'>
+								<Image className={styles.reference} width={30} height={30} src={FB} alt='kolporter facebook' />
 							</Link>
-							<Link onClick={backToTop} href='/'>
-								<Image width={50} height={50} src={INSTA} alt='salonikdlaciebie.pl' />
+							<Link target='_blank' href='https://www.instagram.com/kolporter.com.pl/'>
+								<Image className={styles.reference} width={30} height={30} src={INSTA} alt='kolporter instagram' />
 							</Link>
-							<Link onClick={backToTop} href='/'>
-								<Image width={50} height={50} src={LINKED} alt='salonikdlaciebie.pl' />
+							<Link target='_blank' href='https://pl.linkedin.com/company/kolporter'>
+								<Image className={styles.reference} width={30} height={30} src={LINKED} alt='kolporter linkedin' />
 							</Link>
 						</div>
 					</div>
@@ -68,7 +87,11 @@ export default function Footer() {
 						</ul>
 					</div>
 					<div className={styles.disclaimer}>
-						<p>Masz uwagi do strony? Napisz</p>
+						<p className={styles.description}>Masz uwagi do strony? Skontaktuj się ze mną:</p>
+						<p className={styles.mail} onClick={handleCopy}>
+							{email}
+							{copied && <span className={styles.alert}>Skopiowano!</span>}
+						</p>
 					</div>
 				</div>
 			</Container>
