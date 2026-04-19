99c73906e234a753a1d05c105d3f93ff45227191
Michał Miotła <miotlamichal@gmail.com>
2026-04-19 21:01:42 +0200

    reworking hero section

--- src/app/components/Hero/Hero.js
+++ src/app/components/Hero/Hero.js
@@ -6,40 +6,42 @@ import Image from 'next/image'
 import HeroNewsstand from '../../../../public/newsstand.svg'
 import HeroReading from '../../../../public/reading.svg'
 import HeroGaming from '../../../../public/gaming.svg'
+import HeroImg from '../../../../public/salonik.jpg'
 import { useEffect, useState } from 'react'
 import Button from '../Button/Button'
 
 export default function Hero() {
-	const [currentSlide, setCurrentSlide] = useState(0)
+	// const [currentSlide, setCurrentSlide] = useState(0)
 
-	const slides = [HeroNewsstand, HeroReading, HeroGaming]
+	// const slides = [HeroNewsstand, HeroReading, HeroGaming]
 
-	useEffect(() => {
-		const interval = setInterval(() => {
-			setCurrentSlide(prev => (prev + 1) % slides.length)
-		}, 10000)
+	// useEffect(() => {
+	// 	const interval = setInterval(() => {
+	// 		setCurrentSlide(prev => (prev + 1) % slides.length)
+	// 	}, 10000)
 
-		return () => clearInterval(interval)
-	}, [])
+	// 	return () => clearInterval(interval)
+	// }, [])
 
-	const slide = slides[currentSlide]
+	// const slide = slides[currentSlide]
 
 	return (
 		<div className={styles.hero}>
+			<div className={styles.main_image}></div>
+			<div className={styles.gradient}></div>
 			<Container>
 				<div className={styles.main}>
-					<div className={styles.main_titles}>
-						{/* <Image className={styles.logo} src={Logo} alt='newsstand hero illustration' /> */}
-						<h1 className={styles.title}>KOLPORTER - twoje centrum informacji i drobnych przyjemności :)</h1>
+					{/* <Image className={styles.logo} src={Logo} alt='newsstand hero illustration' /> */}
+					<div className={styles.titles}>
+						<h1 className={styles.title}>Twój punkt informacji i drobnych przyjemności :)</h1>
+						<h2 className={styles.subtitle}>strona dla klientów Saloników Prasowych Kolporter</h2>
+					</div>
+					<div className={styles.buttons}>
 						<Button title={'znajdź salonik'} link={'#map'}></Button>
 						<Button title={'zobacz promocje'} link={'#promocje'}></Button>
 					</div>
-					<div className={styles.main_image_box}>
-						<Image className={styles.main_image} src={slides[currentSlide]} alt='newsstand hero illustration' />
-					</div>
 				</div>
 			</Container>
-			<div className={styles.gradient}></div>
 		</div>
 	)
 }
